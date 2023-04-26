5dfd45ed6832923654a749b2d5a76ac539dd1d21
ktw9807 <ktw9807@naver.com>
2023-04-26 13:33:39 +0900

    Initial commit

---
+++ src/test/java/com/example/petMitsuceta/PetMitsucetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.petMitsuceta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetMitsucetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
